fa2e4c8be33c7cc78c3dd3e8e259be391bbf5260
Meghag5 <@meghagupta05012001.com>
2021-06-09 19:20:00 +0530

    add

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -13,6 +13,12 @@ class Menu extends Component {
         this.state = {
            selectedDish: null
         }
+        console.log('Menu Component constructor is invoked.');
+    }
+
+    //This method will be invoked again and will test when above method is invoked
+    componentDidMount(){
+        console.log('Menu Component componentDidMount is invoked.');
     }
 
     //on clicking this will show description
@@ -47,8 +53,8 @@ class Menu extends Component {
             return(
                 // To render list of items in menu we are going to use media
                 // the tag = li will going to specify list
-                <div className="col-12 col-md-5 m-1">
-                    <Card key={dish.id} onClick = {() => this.onDishSelect(dish)}>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick = {() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay body className="ml-5">
                             <CardTitle>{dish.name}</CardTitle>
@@ -57,6 +63,8 @@ class Menu extends Component {
                 </div>
             );
         });
+
+        console.log('Menu Component render is invoked.');
 
         return(
             <div className="container">
