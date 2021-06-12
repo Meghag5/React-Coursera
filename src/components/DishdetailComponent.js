91cbca8ed8d68baa7aa5cc253ea5038520a09c38
Meghag5 <@meghagupta05012001.com>
2021-06-13 00:57:50 +0530

    Header and Footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,7 +5,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap'
     function RenderDish({dish}){
     if(dish!=null){
         return(
-        <div className="col-12 col-md-5 m-1">
+        <div className="col-12 col-md-5 m-1 ml-5">
             <Card>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardBody>
